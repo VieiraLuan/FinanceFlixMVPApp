588d27fa22a2869c6d8e6d296748611f789e5857
Luan Vieira <luandasilvavieira@hotmail.com>
2024-02-27 22:43:12 -0300

    commit

--- app/src/main/java/com/fiap/financeflix/MainActivity.kt
+++ app/src/main/java/com/fiap/financeflix/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					//Eu escrevi isso
+
+					//Exibindo como trabalhar com os merges
 
 				}
 			}
